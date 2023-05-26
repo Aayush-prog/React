90a6dfc196472c9c46759f6384f57fcd227e4998
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-26 14:01:36 +0545

    components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer>
+        <small>@ 2023 Bombhu. all rights reserved</small>
+      </footer>
+    );
+  }
---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page=(
+  <h1>Test</h1>
+)
+
+ReactDOM.render(page)
+
+export default App;
