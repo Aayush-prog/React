d8d969cedfc59efbd2f7829058b9dffa11fc1ff1
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-27 09:50:37 +0545

    day3

--- AirBnb/component/Card.jsx
+++ AirBnb/component/Card.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
+import React from "react"
 
-export default function Card(){
-    return(
+export default function Card(props) {
+    return (
         <div className="card">
-            <img src="https://i2-prod.bristolpost.co.uk/incoming/article4072173.ece/ALTERNATES/s615b/1_Saltford-house.jpg" className="card-photo"/>
-            <div className="card-info">
-                <img className="star" src="https://github.com/somadisingh/airbnb/blob/master/src/images/star.png?raw=true" />
-                <span>4.3</span>
-                <span>Nepal</span>
-            </div>
-            <p>Made by</p>
-            <p>Bombhu</p>
+            <img src={`${props.img}`} className="card--image" />
+            <p className="card--title">{props.title}</p>
         </div>
     )
 }
--- AirBnb/src/App.jsx
+++ AirBnb/src/App.jsx
@@ -5,16 +5,29 @@ import './App.css'
 import Navbar from '../component/Navbar'
 import Body from '../component/Body'
 import Card from '../component/Card'
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Navbar/>
-      <Body/>
-      <Card/>
-    </>
-  )
+import data from "../component/data"
+export default function App() {
+            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    return (
+        <div>
+            <Navbar />
+            <Body/>
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
-
-export default App
