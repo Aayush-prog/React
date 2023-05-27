713f16e85067f4ffaea589064fb0781997c1dca2
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-27 14:09:26 +0545

    day3

--- AirBnb/component/Card.jsx
+++ AirBnb/component/Card.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 
-export default function Card(props) {
+export default function Card(prop) {
+    let props=prop.item
     return (
         <div className="card">
-            <img src={`${props.img}`} className="card--image" />
+            {props.openSpots===0 && <div className="card--badge">SOLD OUT</div>}
+            <img src={`${props.coverImg}`} className="card--image" />
             <p className="card--title">{props.title}</p>
         </div>
     )
