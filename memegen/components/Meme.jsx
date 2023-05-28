101e3d693dcf1ed3c547dd5200997995d2c31ab7
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-28 17:32:12 +0545

    day4

--- memegen/components/Meme.jsx
+++ memegen/components/Meme.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 export default function Meme(){
-    const[firstValue,setFirstValue]=React.useState("")
+    const[formValue,setFormValue]=React.useState(
+        {firstValue:"",
+        lastValue:""
+    })
+    
     function handleChange(event){
-        setFirstValue(event.target.value)
-        
-
+        const {name,value}=event.target
+        setFormValue(prevValue=>{
+            return {
+                ...prevValue,
+                [name]:value
+            }
+        })
     }
-    console.log(firstValue)
-    function handleClick(){
+    function handleSubmit(event){
+        event.preventDefault()
 
     }
     return(
         <main>
-            <div className="form">
-                <input type="text" className="form-input" onChange={handleChange}/>
-                <input type="text" className="form-input"/>
-                <button className='form--button' onClick={handleClick}>Generate a new image</button>
+            <form className="form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="top" className="form-input" name="firstValue" onChange={handleChange} value={formValue.firstValue}/>
+                <input type="text" placeholder="bottom" value={formValue.lastValue} className="form-input" name="lastValue" onChange={handleChange} />
+                <button className='form--button'>Generate a new image</button>
+            </form>
+            <div className="meme">
+                <img src=""className="meme--image" />
+                <h2 className="meme--text top">{formValue.firstValue}</h2>
+                <h2 className="meme--text bottom">{formValue.lastValue}</h2>
             </div>
         </main>
     )
