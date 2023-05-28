4c8e8e031ad71673317e26c52e119e3bf9e334d6
Aayush-prog <119147531+Aayush-prog@users.noreply.github.com>
2023-05-28 09:42:53 +0545

    day4

--- memegen/components/Meme.jsx
+++ memegen/components/Meme.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 export default function Meme(){
+    const[firstValue,setFirstValue]=React.useState("")
+    function handleChange(event){
+        setFirstValue(event.target.value)
+        
+
+    }
+    console.log(firstValue)
     function handleClick(){
 
     }
     return(
         <main>
             <div className="form">
-                <input type="text" className="form-input"/>
+                <input type="text" className="form-input" onChange={handleChange}/>
                 <input type="text" className="form-input"/>
                 <button className='form--button' onClick={handleClick}>Generate a new image</button>
             </div>
